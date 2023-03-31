f8524609222d55c828d54f3a3820c553fa18204f
leezahuor <leezahuor@gmail.com>
2023-03-31 11:14:58 -0700

    fixed method that adds food and renders lists

---
+++ src/components/Food/FoodList.js
@@ -0,0 +1,35 @@
+// function FoodList({ foods }) {
+//   return (
+//     <div>
+//       <h1>Current Foods Available</h1>
+//       <ul>
+//         {/* {foods.map((food) => ( */}
+//           <li>
+//             <div>
+//               <h2>{food.name}</h2>
+//             </div>
+//           </li>
+//         {/* ))} */}
+//       </ul>
+//     </div>
+//   );
+// }
+
+function FoodList() {
+    return (
+      <div>
+        <h1>Current Foods Available</h1>
+        <ul>
+            <li>
+                Banana
+            </li>
+            <li>
+                Apple
+            </li>
+        </ul>
+      </div>
+    );
+  }
+  
+
+export default FoodList;
